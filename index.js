565ed865b6d7ca79af3b40e3016facca5c8704d9
Diana <diana@multiplica.com>
2022-03-09 20:46:56 -0500

    change license

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert("Te amo chami!");
+const messages = ["Chami", "Macuqui", "Chamaquin", "Diana", "Kevin"];
+
+const randomMsg = () => {
+  const message = messages[Math.floor(Math.random() * messages.length)];
+  console.log(message);
+};
+
+module.exports = { randomMsg };
